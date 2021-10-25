c3547e77a221fca3a8e113737a431b09ebb26aa5
SpicyTaterTot00 <mcfarland_jessica1@columbusstate.edu>
2021-10-25 21:21:15 +0000

    Commit_nodept3

--- index.js
+++ index.js
@@ -5,16 +5,32 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended : true}));
 
+var tasks = ['Wake up','Eat breakfast'];
+var completed = [];
+
 app.get('/', function(request, response){
-    response.render('index');
+    response.render('index', {tasks:tasks, completed:completed});
     //response.send('Hello World!');
 })
 
 app.post('/addToDo', function(req,res){
-    res.send('hello')
+    tasks.push(req.body.newtodo)
+    res.redirect('/');
 })
 
 app.post('/removeToDo', function(req,res){
+    const remove= req.body.check;
+    if(typeof remove === 'string'){
+        tasks.splice(tasks.indexOf(remove), i)
+    }else if(typeof remove === 'object'){
+        for(var i=0; i<remove.length; i++){
+            tasks.splice(tasks.indexOf(remove[i]), i)
+        }
+    }
+    res.redirect('/');
+})
+
+app.post('/deleteToDo', function(req,res){
     res.send('world')
 })
 
