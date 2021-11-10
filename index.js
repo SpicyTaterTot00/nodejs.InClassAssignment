7be91c5205314e2978b1c80058bff55397120226
SpicyTaterTot00 <mcfarland_jessica1@columbusstate.edu>
2021-11-10 20:01:17 +0000

    commit

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let express = require('express');
 let bodyParser= require('body-parser');
 let mongoose = require('mongoose');
 const ToDo = require('./models/todo.model');
-const Zip = require('./models/zip.model')
+//const Zip = require('./models/zip.model')
 
 var app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended : true}));
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 //Connection to Mongo
 const mongoDB = 'mongodb+srv://admin:admin@cluster0.q756k.mongodb.net/todo?retryWrites=true&w=majority';
@@ -101,9 +101,9 @@ app.get('/zip', (req, res) => {
     res.sendFile(__dirname + '/views/page2.ejs')
  })
 
- app.post('/zip', function(req,res){
+//  app.post('/zip', function(req,res){
     
-    })
+//     })
     
 app.listen(3000, function(){
     console.log('App is running on port 3000');
